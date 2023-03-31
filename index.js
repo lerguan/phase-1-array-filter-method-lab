030d64f36a113df217c630d4194375b45cb823c2
Leroy G <liguan698@gmail.com>
2023-03-31 10:50:12 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+   return array.filter(function(item){
+        return item.toUpperCase() === string.toUpperCase()
+    })
+}
+
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(item){
+        return item.slice(0, string.length) === string
+    })
+}
+
+function matchName(array, string){
+   return array.filter(function(item){
+        return item.name === string
+    })
+}
